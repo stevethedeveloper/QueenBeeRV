a91d4ae40b672ac80b6612d5c81621a91c91f857
stevethedeveloper <info@stevethedeveloper.com>
2023-08-03 18:05:39 -0700

    Add template models

---
+++ QueenBeeRV/Models/TemplateItem.swift
@@ -0,0 +1,12 @@
+//
+//  TemplateItems.swift
+//  QueenBeeRV
+//
+//  Created by Stephen Walton on 8/3/23.
+//
+
+import Foundation
+
+struct TemplateItem: Codable {
+    var title: String
+}
---
+++ QueenBeeRV/Models/Templates.swift
@@ -0,0 +1,14 @@
+//
+//  Templates.swift
+//  QueenBeeRV
+//
+//  Created by Stephen Walton on 8/3/23.
+//
+
+import Foundation
+
+struct Templates: Codable {
+    var title: String
+    var description: String
+    var items: [TemplateItem]
+}
