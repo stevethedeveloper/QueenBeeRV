c15d19728f028a0458aca4a0c43a49c6584cf675
stevethedeveloper <info@stevethedeveloper.com>
2023-09-05 16:42:02 -0700

    Relocating files and removing old Xcode stuff

--- QueenBeeRV/Controllers/BlogListVC.swift
+++ QueenBeeRV/Controllers/BlogListVC.swift
@@ -8,67 +8,78 @@
 import UIKit
 
 class BlogListVC: UIViewController {
-    let viewModel = BlogListViewModel()
-    var tableView = UITableView()
+    private let viewModel = BlogListViewModel()
+    private var tableView: UITableView = {
+        let table = UITableView()
+        table.separatorStyle = UITableViewCell.SeparatorStyle.none
+        table.rowHeight = 130
+        table.register(PostCell.self, forCellReuseIdentifier: PostCell.identifier)
+        return table
+    }()
     
     override func loadView() {
         let view = UIView()
         self.view = view
-        configureTableView()
+        view.addSubview(tableView)
+        tableView.delegate = self
+        tableView.dataSource = self
+        setTableViewConstraints()
         viewModel.getPosts()
     }
-    
+        
     override func viewDidLoad() {
         super.viewDidLoad()
-        title = "Blog"
+        title = "Articles"
+        navigationController?.navigationBar.prefersLargeTitles = false
+        navigationController?.setStatusBar(backgroundColor: UIColor(named: "MenuColor")!)
 
         viewModel.blogPosts.bind { [weak self] _ in
             DispatchQueue.main.async {
                 self?.tableView.reloadData()
             }
         }
+        
+        viewModel.onErrorHandling = { error in
+            DispatchQueue.main.async {
+                let ac = UIAlertController(title: "An error occured", message: error, preferredStyle: .alert)
+                ac.addAction(UIAlertAction(title: "OK", style: .default))
+                self.present(ac, animated: true)
+            }
+        }
     }
         
-    func configureTableView() {
-        view.addSubview(tableView)
-        tableView.delegate = self
-        tableView.dataSource = self
-        tableView.rowHeight = 100
-        tableView.separatorColor = .systemGray
-        tableView.register(PostCell.self, forCellReuseIdentifier: "PostCell")
-        tableView.pin(to: view)
-    }
-    
-    func showError() {
-        DispatchQueue.main.async {
-            let ac = UIAlertController(title: "Loading error", message: "There was a problem loading the feed; please check your connection and try again.", preferredStyle: .alert)
-            ac.addAction(UIAlertAction(title: "OK", style: .default))
-            self.present(ac, animated: true)
-        }
+    private func setTableViewConstraints() {
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+        
     }
 }
 
-// MARK: Data delegate and datasource functions
+// MARK: Delegate and datasource functions
 extension BlogListVC: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return viewModel.blogPosts.value.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostCell
+        let cell = tableView.dequeueReusableCell(withIdentifier: PostCell.identifier) as! PostCell
         cell.separatorInset = .zero
+        cell.selectionStyle = .none
         let post = viewModel.blogPosts.value[indexPath.row]
         cell.set(post: post)
         return cell
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let vc = BlogPostVC()
-//        vc.viewModel = BlogPostViewModel()
+        let vc = WebviewVC()
         vc.viewModel.currentWebsite.value = viewModel.blogPosts.value[indexPath.row].url
-        
-//        vc.modalPresentationStyle = .fullScreen
-//        vc.currentWebsite = viewModel.blogPosts.value[indexPath.row].url
-        navigationController?.pushViewController(vc, animated: true)
+        vc.modalPresentationStyle = .pageSheet
+        vc.modalTransitionStyle = .coverVertical
+        self.present(vc, animated: true)
     }
 }
